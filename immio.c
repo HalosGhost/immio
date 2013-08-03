bdcdaef65d178cb54e05d781acfd4d498586db5c
HalosGhost <halosghost@archlinux.info>
2013-08-03 15:32:25 -0400

    Overhaul help message;add tentative options;fix license formatting

--- immio.c
+++ immio.c
@@ -1,7 +1,7 @@
-/*******************************************\
-* Simple program to upload images to imm.io *
-* License: CCPL Sam Stuewe, 2013 (CC-BY-SA) *
-\*******************************************/
+/****************************************\
+* Upload and manage images on imm.io     *
+* License: CCPL Sam Stuewe, 2013 (BY-SA) *
+\****************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,13 +9,22 @@
 #include <unistd.h>
 
 FILE *url;
-char command[256];
-char output[256];
+char command[256],output[256];
 int len,i;
 int n=16;
+int v=0;
 
 void usage(char *progname) {
-	fprintf(stderr,"Usage: %s [-h|--help] [-d|--direct] <file1 [file2 ... fileN]>\n",progname);
+	fprintf(stderr,"Usage: %s [-d|-v] image(s)\n",progname);
+	fprintf(stderr,"   or: %s -r id token\n",progname);
+	fprintf(stderr,"   or: %s -m id token name\n\n",progname);
+	fprintf(stderr,"All arguments are mandatory for removing and renaming.\n");
+	fprintf(stderr,"  -d   post direct links to images rather\n         than to landing pages.\n");
+	fprintf(stderr,"  -h   print this help message.\n");
+	fprintf(stderr,"  -r   remove identified image.\n");
+	fprintf(stderr,"  -m   rename identified image.\n");
+	fprintf(stderr,"  -v   be more verbose when printing\n         image information.\n\n");
+	fprintf(stderr,"See `man %s` for more information.\n",progname);
 	exit(44);
 }
 
@@ -33,8 +42,27 @@ int main(int argc, char** argv) {
 	if (argv[1]==NULL) usage(argv[0]);
 	for (i = 1; i < argc; i++) {
 		if (argv[i][0]=='-') {
-			if (argv[i][1]=='h'||argv[i][2]=='h') usage(argv[0]);
-			else if (argv[i][1]=='d'||argv[i][2]=='d') n=12;
+			if (argv[i][1]=='\0') usage(argv[0]);
+
+			else if (argv[i][1]=='h') usage(argv[0]);
+
+			else if (argv[i][1]=='d') n=12;
+
+			else if (argv[i][1]=='r') {
+				if (argv[i+1]==NULL||argv[i+2]==NULL) usage(argv[0]);
+			    else { fprintf(stderr,"This option is still under construction.\n"); return 1; }
+					 // post id with associated token
+			}
+
+			else if (argv[i][1]=='m') {
+				if (argv[i+1]==NULL||argv[i+2]==NULL||argv[i+3]==NULL) usage(argv[0]);
+				else { fprintf(stderr,"This option is still under construction.\n"); return 1; }
+					 // post id with associated token and new name
+			}
+
+			else if (argv[i][1]=='v') { v=1; fprintf(stderr,"Verbosity has not yet been implemented.\n"); }
+
+			else fprintf(stderr,"Unrecognized option.\nSee `%s -h` or `man %s` for help.\n",argv[0],argv[0]);
 		}
 		else {
 			upload(argv[i],n);
